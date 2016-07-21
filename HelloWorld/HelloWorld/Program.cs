051b0aa7114be0521a168495d53fd23e5113c75b
Quesada <abel.quesada@covidien.com>
2016-07-21 17:45:03 -0600

    mi primer hola mundo en C#

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld /* es como el apellido del programa para separar codigo */
+{
+    class Program
+    {
+        static void Main(string[] args) /*cuando el programa arranca ejecuta siempre la funcion de main, aqui esta definiendo un arreglo */
+        {
+            Console.WriteLine("Hello World");
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
