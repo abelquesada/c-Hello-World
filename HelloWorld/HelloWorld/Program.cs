0eefe8fc9d01cd1bf407624173c0d98515c9b89b
Quesada <abel.quesada@covidien.com>
2016-07-21 18:14:35 -0600

    agrego comentarios

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -6,12 +6,14 @@ using System.Threading.Tasks;
 
 namespace HelloWorld /* es como el apellido del programa para separar codigo */
 {
+
     class Program
     {
         static void Main(string[] args) /*cuando el programa arranca ejecuta siempre la funcion de main, aqui esta definiendo un arreglo */
         {
             Console.WriteLine("Hello World");
             Console.WriteLine("Press any key to exit");
+            //la linea de abajo lee una tecla
             Console.ReadKey();
         }
     }
